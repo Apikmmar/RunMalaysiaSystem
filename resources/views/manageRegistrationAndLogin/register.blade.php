34a3a8266978e752587284318754c2d24f14dc44
apikammar <apikammar07@gmail.com>
2023-12-24 13:26:14 +0800

    update style login and register

--- resources/views/manageRegistrationAndLogin/register.blade.php
+++ resources/views/manageRegistrationAndLogin/register.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="{{ asset('images/proj-laravel-logo-no-bg.png') }}" type="image/png">
-    <link rel="stylesheet" href="{{ asset('css/stylelogin.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>RunMalaysia</title>
-</head>
-<body class="bodyclass">
+@extends('components.master2')
+
+@section('inside')
+    
     <br>
     <div id="video-container">
         <video src="{{ asset('video/cinematic-running.mp4') }}" autoplay loop muted class="playvid2"></video>
@@ -36,11 +28,10 @@
                 <br>
                 <input type="submit" name="login" value="Login" class="btn btn-primary">
                 <br>
-                <p class="text-light">Already have an account? <a href="/login">Click Here</a></p>
+                <p class="text-dark">Already have an account? <a href="/login">Click Here</a></p>
             </div>
         </form>
     </div>
     <br>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-</html>
+
+@endsection
